33e6474107e3db1b998a63a93853581d8c59082f
Gokul Kumarresen <gokulk@live.com>
2016-11-15 17:01:33 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'); 
 var bodyParser = require("body-parser"); 
+var _ = require("underscore"); 
 
 var app = express(); 
 var PORT = process.env.PORT || 3000; 
@@ -38,13 +39,14 @@ app.get("/todos", function(req, res){
 
 app.get("/todos/:id", function(req, res){
 	var todoId = parseInt(req.params.id); 
-	var matchedTodo; 
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
 	//res.send(todos[todoId].description); 
-	todos.forEach(function(item){
+	/*todos.forEach(function(item){
 		if (item.id === todoId){
 			matchedTodo = item; 
 		}
-	});
+	});*/
 
 	if (matchedTodo){
 		res.json(matchedTodo); 
@@ -57,7 +59,17 @@ app.get("/todos/:id", function(req, res){
 
 //POST /todos/:id 
 app.post('/todos', function(req, res){
-	var body = req.body; 
+	//var body = req.body; 
+	var body = _.pick(req.body, "description", "completed");
+
+	if (!_.isBoolean(body.completed)
+		|| !_.isString(body.description)
+		|| body.description.trim().length === 0){
+		return res.status(400).send(); 
+	}
+
+	body.description = body.description.trim(); 
+
 	body.id = todoNextId++; 
 	todos.push(body); 
 
